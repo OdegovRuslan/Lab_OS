9818089bb548addcec70a2316654b7dbc301b612
OdegovRuslan <46085563+OdegovRuslan@users.noreply.github.com>
2018-12-25 11:14:09 +0300

    Update out.c

--- 4.2/out.c
+++ 4.2/out.c
@@ -12,14 +12,14 @@ int main(){
     struct sembuf close = {0,-1,0};
     struct sembuf open = {0,1,0};
 
-    key_t key = ftok("sm", 30);
-    int shm = shmget(key, 100, IPC_CREAT|0666);
+    key_t key = 2002 ;
+    int shm = shmget(key, 32, IPC_CREAT|0666);
     if (shm == -1){
         printf("shmget error \n");
         return 0;
     }
 
-    int sm = shmget(key, 100, IPC_CREAT|0666);
+    int sm = shmget(key, 32, IPC_CREAT|0666);
     if (sm == -1){
         printf("shmget error \n");
         return 0;
